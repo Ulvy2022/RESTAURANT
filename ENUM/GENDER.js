7ab94bef27ac3360b4224100f5b0584d8c5d1772
Ulvy2022 <ulvy.romy@student.passerellesnumeriques.org>
2022-06-30 22:48:12 +0700

    fila days

--- ENUM/GENDER.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Gender = void 0;
-var Gender;
-(function (Gender) {
-    Gender["Male"] = "Male";
-    Gender["Female"] = "Female";
-})(Gender = exports.Gender || (exports.Gender = {}));
